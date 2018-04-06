8f3ee8b26cf3964f0ae15062ce3021395b3579af
hazzindu <hazzindu@gmail.com>
2018-04-06 16:27:05 +0530

    Adding profle textbox - add profle

--- src/main/java/com/amazonaws/codedeploy/AWSCodeDeployPublisher.java
+++ src/main/java/com/amazonaws/codedeploy/AWSCodeDeployPublisher.java
@@ -84,6 +84,7 @@ public class AWSCodeDeployPublisher extends Publisher implements SimpleBuildStep
     private static final Regions[] AVAILABLE_REGIONS                 = {Regions.AP_NORTHEAST_1, Regions.AP_SOUTHEAST_1, Regions.AP_SOUTHEAST_2, Regions.EU_WEST_1, Regions.US_EAST_1, Regions.US_WEST_2, Regions.EU_CENTRAL_1, Regions.US_WEST_1, Regions.SA_EAST_1, Regions.AP_NORTHEAST_2, Regions.AP_SOUTH_1, Regions.US_EAST_2, Regions.CA_CENTRAL_1, Regions.EU_WEST_2, Regions.CN_NORTH_1};
 
     private final String  s3bucket;
+    private final String  profile;
     private final String  s3prefix;
     private final String  githubRepository;
     private final String  githubCommitId;
